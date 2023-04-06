e9606dd93f0ea2ee9cf55c5c8afe72197b7b0765
Zach Veenstra <zach.veen.gr@gmail.com>
2023-04-06 14:33:09 -0400

    Added firebase credentials

---
+++ rent-a-bike/firebase/fb-credentials.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDzPwe3_x4_PSwi5qZDei-FSAbEQeXHE7k",
+    authDomain: "rentabike-b3dcc.firebaseapp.com",
+    databaseURL: "https://rentabike-b3dcc-default-rtdb.firebaseio.com",
+    projectId: "rentabike-b3dcc",
+    storageBucket: "rentabike-b3dcc.appspot.com",
+    messagingSenderId: "841208401258",
+    appId: "1:841208401258:web:c3efbbe9cc6f46a3896913",
+    measurementId: "G-JD0YDQYCVS"
+};
